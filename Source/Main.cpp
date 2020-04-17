abdc9f25fc422f80e5dbe4cb935f3d02ab6937ba
Nacho Cortizo <nachocpol@gmail.com>
2020-04-17 22:04:20 +0100

    Initial setup before the jam starts.

---
+++ Source/Main.cpp
@@ -0,0 +1,58 @@
+#include "Graphics.h"
+#include "Sprite.h"
+#include "Input.h"
+#include "Image.h"
+#include "Time.h"
+#include "FileUtils.h"
+using namespace Waffle;
+
+int main()
+{
+	// Add the file device for this project:
+	FileSystem::Get().AddFileDevice({ "Projects/LD46/Data/",FileDevice::Type::Assets,"data" });
+
+	float totalTime = 0.0f;
+	float deltaTime = 0.0f;
+
+	Timer timer;
+	timer.Start();
+
+	Graphics& graphics = Graphics::Get();
+	graphics.Init();
+
+	Image* img = Image::CreateFromFile("data:Grid.png");
+
+	// Create one sprite and assign an image:
+	Sprite spriteImg(512, 256, img);
+	spriteImg.SetPosition(200.0f, 50.0f);
+	float rot = 0.0f;
+
+	// Simple sprite (just tint)
+	Sprite spriteImg2(512, 256);
+	spriteImg2.SetTint(0.8f, 0.25f, 0.0f);
+	spriteImg2.SetPosition(-200.0f, -50.0f);
+
+	while (!graphics.Closed())
+	{
+		timer.Start();
+
+		rot += 25.0f * deltaTime;
+		spriteImg.SetRotation(rot);
+		spriteImg2.SetRotation(-rot);
+
+		if (Input::GetKeyPressed(Key::Space))
+		{
+			rot = 0.0f;
+		}
+
+		graphics.ClearScreen(0.5f, 0.5f, 0.5f, 1.0f);
+		graphics.DrawSprite(&spriteImg2);
+		graphics.DrawSprite(&spriteImg);
+		graphics.Flush();
+
+		// Update frame time:
+		deltaTime = timer.Stop() / 1000.0f;
+		totalTime += deltaTime;
+	}
+	return 0;
+}
